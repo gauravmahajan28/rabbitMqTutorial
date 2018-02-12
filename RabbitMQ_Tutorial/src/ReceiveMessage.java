70f08495bc6425e8c6fc406d960a6a5465546fca
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-02-12 15:46:36 +0530

    initial commit

---
+++ RabbitMQ_Tutorial/src/ReceiveMessage.java
@@ -0,0 +1,9 @@
+
+public class ReceiveMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RabbitMQ_Tutorial/src/SendMessage.java
@@ -0,0 +1,9 @@
+
+public class SendMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
